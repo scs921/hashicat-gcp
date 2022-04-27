eee9b9c55a642dc6ae6b2e4d77ac84fb81b1ccf2
ssprouse <samsprouse23@gmail.com>
2022-04-27 13:29:25 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,13 @@
+module "vpc" {
+    source  = "terraform-google-modules/network/google"
+    version = "~> 3.4"
+
+    project_id   = var.project
+    network_name = "example-vpc"
+
+    subnets = [
+        {
+            subnet_name           = "subnet-01"
+            subnet_ip             = "10.10.10.0/24"
+            subnet_region         = var.region
+        }
